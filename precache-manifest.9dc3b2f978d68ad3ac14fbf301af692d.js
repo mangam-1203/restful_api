44f10d2c5989e9f0a417d82dceb07384f53bacdd
mangam-1203 <43307447+mangam-1203@users.noreply.github.com>
2019-03-05 15:27:54 +0530

    Updates

---
+++ precache-manifest.9dc3b2f978d68ad3ac14fbf301af692d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "68054a8f9b060c6b5865",
+    "url": "/static/js/main.68054a8f.chunk.js"
+  },
+  {
+    "revision": "aebb94cdc0cde738189c",
+    "url": "/static/js/2.aebb94cd.chunk.js"
+  },
+  {
+    "revision": "68054a8f9b060c6b5865",
+    "url": "/static/css/main.24113a0c.chunk.css"
+  },
+  {
+    "revision": "378833d733247b08a4f37481d69ff7b6",
+    "url": "/index.html"
+  }
+];
