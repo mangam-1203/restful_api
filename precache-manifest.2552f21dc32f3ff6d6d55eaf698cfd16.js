a51d34fa715812f03e7c560e89606f084b62441f
mangam-1203 <43307447+mangam-1203@users.noreply.github.com>
2019-03-05 18:14:18 +0530

    Updates

---
+++ precache-manifest.2552f21dc32f3ff6d6d55eaf698cfd16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ace798171155b6d0a7a",
+    "url": "/phone-directory/static/js/runtime~main.3ace7981.js"
+  },
+  {
+    "revision": "68054a8f9b060c6b5865",
+    "url": "/phone-directory/static/js/main.68054a8f.chunk.js"
+  },
+  {
+    "revision": "aebb94cdc0cde738189c",
+    "url": "/phone-directory/static/js/2.aebb94cd.chunk.js"
+  },
+  {
+    "revision": "68054a8f9b060c6b5865",
+    "url": "/phone-directory/static/css/main.24113a0c.chunk.css"
+  },
+  {
+    "revision": "e7209f455851f1e3f2f24d74f7015930",
+    "url": "/phone-directory/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9dc3b2f978d68ad3ac14fbf301af692d.js"
+  "/phone-directory/precache-manifest.2552f21dc32f3ff6d6d55eaf698cfd16.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/phone-directory/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
